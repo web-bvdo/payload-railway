57a418de6bc3c56079073edc7205f1f260878562
DaanvanMaren <daan2003@outlook.com>
2026-07-13 12:22:10 +0200

    Add initial migration for media and user session tables in SQLite

---
+++ src/migrations/index.ts
@@ -0,0 +1,9 @@
+import * as migration_20260713_101736 from './20260713_101736';
+
+export const migrations = [
+  {
+    up: migration_20260713_101736.up,
+    down: migration_20260713_101736.down,
+    name: '20260713_101736'
+  },
+];
